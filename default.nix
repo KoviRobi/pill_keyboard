25261f23dbd9681c3f5b65726193d93e211f4a7d
Kovacsics Robert <rmk35@cam.ac.uk>
2020-03-02 02:40:02 +0000

    Added SysTick, it now blinks the LEDs and we use WFI

--- default.nix
+++ default.nix
@@ -1,6 +1,7 @@
 { stdenv
 , lib
 , libopencm3
+, gnumake # TODO: only because of the info page
 , gcc-arm-embedded
 , Literate
 , cppcheck
@@ -9,11 +10,11 @@
 , dfu-util
 }:
 
-stdenv.mkDerivation {
+stdenv.mkDerivation rec {
   name = "pill_keyboard";
   src = ./src;
 
-  nativeBuildInputs = [ stlink dfu-util ];
+  nativeBuildInputs = [ gnumake stlink dfu-util ];
   buildInputs = [
     cppcheck
     pandoc
@@ -28,4 +29,15 @@ stdenv.mkDerivation {
     maintainers = with maintainers; [ kovirobi ];
     license = licenses.mit;
   };
+
+  shellHook = let
+    inputs = stdenv.defaultBuildInputs ++ stdenv.defaultNativeBuildInputs ++
+             buildInputs ++ nativeBuildInputs;
+    infoDrvs = builtins.filter (drvP: lib.isDerivation drvP) inputs;
+    infoOutputs = map (drv: lib.getOutput "info" drv) infoDrvs;
+    infoPaths = map (drv: "${drv}/share/info") infoOutputs;
+    #extantInfoPaths = builtins.filter (p: builtins.pathExists (builtins.toPath p)) infoPaths;
+  in ''
+    INFOPATH=${lib.concatStringsSep ":" infoPaths}''${INFOPATH:+:}$INFOPATH
+  '';
 }
